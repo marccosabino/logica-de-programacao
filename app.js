cd4aa5cd46527055cfb43236c7a5f73f77bf6072
marccosabino <marco.sabinosr@gmail.com>
2026-01-29 16:38:18 -0300

    Aula sobre funções

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
 function verificarChute() {
   console.log('O botão foi clicado!');
 }
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = 'Escolha um número entre 1 e 10';
+}
+
+exibirTextoNaTela('h1', 'Hora do Desafio!');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+
+//Exercícios adicionais
+function amoJs() {
+  alert('Eu amo JavaScript!');
+}
+
+function prompt() {}
