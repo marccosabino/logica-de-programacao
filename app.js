8258d014a6352c8d06e0bdcd6cc94f90ffbf786b
marccosabino <marco.sabinosr@gmail.com>
2026-01-30 17:24:57 -0300

    02. Funções

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
 function verificarChute() {
-  console.log('O botão foi clicado!');
+  let chute = document.querySelector('input').value;
+  console.log(chute == numeroSecreto);
 }
 
 function exibirTextoNaTela(tag, texto) {
@@ -10,10 +13,23 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela('h1', 'Hora do Desafio!');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10) + 1;
+}
 
-//Exercícios adicionais
+//4. Crie uma função que exiba um alerta
+// com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
 function amoJs() {
   alert('Eu amo JavaScript!');
 }
 
 function prompt() {}
+
+//6. Ao clicar no botão soma, peça 2 números
+// inteiros e exiba o resultado da soma em um alerta.
+function somandoDoisNumeros() {
+    let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+    let segundoNumero = parseInt(prompt('Digite o segundo número'));
+    let resultado = primeiroNumero + segundoNumero;
+    alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
+}
